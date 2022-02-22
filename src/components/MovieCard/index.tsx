7435f9dbb71e004520dda1de7eb1fe024205b8e2
MajoLkc <lukac.1majo@gmail.com>
2022-02-22 22:53:51 +0100

    Added redirection button to movie details page

--- src/components/MovieCard/index.tsx
+++ src/components/MovieCard/index.tsx
@@ -1,23 +1,53 @@
 import { Card } from "antd"
+import { Link } from "react-router-dom"
 import styled from "styled-components"
+import { HeartOutlined, InfoCircleOutlined } from "@ant-design/icons"
+import { useState } from "react"
+
 const { Meta } = Card
 
 const StyledCard = styled(Card)`
   width: 240px;
+  display: inline-block;
+  height: 450px;
 `
 
-type Props = {
+interface MovieCardProps {
+  id: string
   alt: string
   src: string
   title: string
   type: string
 }
 
-const MovieCard = ({ alt, src, title, type }: Props) => (
-//   <StyledCard hoverable style={{ width: 240 }} cover={<img alt={alt} src={src} />}>
-  <StyledCard hoverable cover={<img alt={alt} src={src} />}>
-    <Meta title={title} description={type} />
-  </StyledCard>
-)
+const style = {
+  color: "red",
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({ id, alt, src, title, type }) => {
+  const [isFavorite, setIsFavorite] = useState<boolean>(false)
+  const toggleFavoritesHandler = () => {
+    setIsFavorite(!isFavorite)
+  }
+
+  return (
+    <StyledCard
+      id={id}
+      cover={<img alt={alt} src={src} />}
+      actions={[
+        <Link to={`/detail/${id}`}>
+          <InfoCircleOutlined key="info" />
+        </Link>,
+        <HeartOutlined
+          key="heart"
+          onClick={toggleFavoritesHandler}
+          style={isFavorite ? style : undefined}
+        />,
+      ]}
+    >
+      <Meta title={title} description={type} />
+    </StyledCard>
+  )
+}
 
 export default MovieCard
