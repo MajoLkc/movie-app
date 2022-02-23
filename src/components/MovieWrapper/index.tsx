b50d1acb0c11fc2b1286225d15d11f542c1a8142
MajoLkc <lukac.1majo@gmail.com>
2022-02-23 19:13:56 +0100

    Changed movie wrapper width

--- src/components/MovieWrapper/index.tsx
+++ src/components/MovieWrapper/index.tsx
@@ -1,10 +1,7 @@
 import styled from "styled-components"
 
 const Wrapper = styled.div`
-  width: 1000px;
-  div:first-of-type {
-    //   margin: 10px;
-  }
+  width: 100%;
 `
 
 const MovieWrapper: React.FC = (props) => <Wrapper>{props.children}</Wrapper>
