af3d10cd14746f82530543cfacfd96019eb07c10
MajoLkc <lukac.1majo@gmail.com>
2022-02-23 20:28:39 +0100

    Color changes in search component

--- src/pages/Search/SearchInput.tsx
+++ src/pages/Search/SearchInput.tsx
@@ -3,8 +3,32 @@ import { useSelector } from "react-redux"
 import Info from "../../components/Info"
 import { useAction } from "../../hooks/useAction"
 import Results from "../../components/Results"
+import styled from "styled-components"
 
 const { Search } = Input
+
+const StyledSearch = styled(Search)`
+  .ant-btn-primary {
+    border-color: red;
+  }
+  .ant-btn-primary:hover {
+    border-color: black;
+  }
+  .ant-input:hover {
+    border-color: red;
+  }
+  .ant-input:focus {
+    border-color: red;
+    box-shadow: none;
+  }
+  button {
+    background-color: red;
+  }
+  button:hover {
+    background-color: black;
+    color: red;
+  }
+`
 
 const SearchInput: React.FC = () => {
   const { searchMovies } = useAction()
@@ -15,7 +39,7 @@ const SearchInput: React.FC = () => {
 
   return (
     <>
-      <Search
+      <StyledSearch
         placeholder="Enter film name here"
         enterButton="Search"
         size="large"
