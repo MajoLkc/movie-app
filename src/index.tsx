a9709fac66948f10f19effcf648ec91b35b874fb
MajoLkc <lukac.1majo@gmail.com>
2022-02-23 15:31:09 +0100

    Console error removal

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
-import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App/App"
 import reportWebVitals from "./reportWebVitals"
 import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById("root")
 )
 
